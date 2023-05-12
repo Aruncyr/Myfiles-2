8ea1407ee87d046b13652291e86f47c2edb1f99a
arun <aruncyrtech@gmail.com>
2023-05-12 10:47:58 +0530

    h2  database example

---
+++ springbooth2database_project/src/main/java/com/test/springbooth2/Springbooth2databaseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.springbooth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbooth2databaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbooth2databaseProjectApplication.class, args);
+	}
+
+}
---
+++ springbooth2database_project/src/test/java/com/test/springbooth2/Springbooth2databaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.springbooth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbooth2databaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
