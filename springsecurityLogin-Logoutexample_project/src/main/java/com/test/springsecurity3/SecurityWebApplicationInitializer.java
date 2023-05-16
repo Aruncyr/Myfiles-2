e4e75548293df63d39d65a6c6db06ba48a5a09db
arun <aruncyrtech@gmail.com>
2023-05-16 17:15:05 +0530

    springsecurity example login&logout

---
+++ springsecurityLogin-Logoutexample_project/src/main/java/com/test/springsecurity3/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.test.springsecurity3;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+	
+}
