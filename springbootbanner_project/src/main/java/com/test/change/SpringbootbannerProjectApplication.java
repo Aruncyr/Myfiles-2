71758bc48d81bcc6cce0352385368b5f8fecec69
arun <aruncyrtech@gmail.com>
2023-05-24 20:54:54 +0530

    how to change banner in springboot

---
+++ springbootbanner_project/src/main/java/com/test/change/SpringbootbannerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.change;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbannerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbannerProjectApplication.class, args);
+	}
+
+}
---
+++ springbootbanner_project/src/test/java/com/test/change/SpringbootbannerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.change;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbannerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
